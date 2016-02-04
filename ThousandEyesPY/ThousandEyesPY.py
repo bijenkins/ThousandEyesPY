dbe357467dcc606be505e87aa7ea0fee1a479667
William (Billy) Jenkins <bilyrocker33@gmail.com>
2016-02-03 22:57:47 -0600

    validation for username and password, cleanup of code

--- ThousandEyesPY/ThousandEyesPY.py
+++ ThousandEyesPY/ThousandEyesPY.py
@@ -3,14 +3,18 @@ import json
 
 class ThousandEyesPY(object):
     """docstring for ThousandEyesAPI"""
-    def __init__(self, USERNAME, PASSWORD):
-        self.USERNAME = USERNAME
-        self.PASSWORD = PASSWORD
+    def __init__(self, username=None, password=None):
+        if (username and not password) or (not username and password):
+            raise ValueError('Must provide username and password for ThousandEyes Service')
+        elif not username and not password:
+            raise ValueError('Must provide username and password for ThousandEyes Service')
+        else:
+            self.username = username
+            self.password = password
 
     def THOUSANDEYES_API_URL(self):
         """ Returns current THOUSANDEYES_API_URL"""
         return "https://api.thousandeyes.com/"
-
 
     def _generate_window(self, window_integer=None, window_unit= None):
         """
@@ -58,18 +62,17 @@ class ThousandEyesPY(object):
         """
         id is required to be a integer.
         """
-        if not isinstance(id, int) and aid is not None:
+        if not isinstance(id, int) and id is not None:
             raise ValueError('Required Integer or None')
         return str(id)
-    def active_alerts(self, window_integer=None, window_unit=None, output_format='json', aid=None):
+
+    def active_alerts(self, window_integer=None, window_unit=None, aid=None):
         """
         Access all current alerts for time filter provided, if no time filter
         is provided it will return all active alerts.
         """
         window = self._generate_window(window_integer=window_integer, window_unit=window_unit)
-
-        output_format = self._format_validation(output_format=output_format)
-
+        output_format = self._format_validation()
         aid = self._aid_validation(aid=aid)
 
         payload = {
@@ -78,22 +81,20 @@ class ThousandEyesPY(object):
                         'aid:': aid
                   }
         print self.USERNAME, self.PASSWORD
-        r = requests.get(self.THOUSANDEYES_API_URL() + 'alerts', auth=(self.USERNAME, self.PASSWORD), params=payload)
+        r = requests.get(self.THOUSANDEYES_API_URL() + 'alerts', auth=(self.username, self.password), params=payload)
         # Use requests built in Raise for Status of 400
         r.raise_for_status()
-        
+
         j = json.loads(r.text)
         # print json.dumps(j, indent=4)
         return j
 
-    def active_alert_detail(self, alert_id=None, output_format='json', aid=None):
+    def active_alert_detail(self, alert_id=None, aid=None):
         """
         Recieve the details of a specific Active Alert
         """
         alert_id = self._id_validation(id=alert_id)
-
-        output_format = self._format_validation(output_format=output_format)
-
+        output_format = self._format_validation()
         aid = self._aid_validation(aid=aid)
 
         payload = {
@@ -101,7 +102,7 @@ class ThousandEyesPY(object):
                         'aid:': aid
                   }
 
-        r = requests.get(self.THOUSANDEYES_API_URL() + 'alerts/' + alert_id, auth=(self.USERNAME, self.PASSWORD), params=payload)
+        r = requests.get(self.THOUSANDEYES_API_URL() + 'alerts/' + alert_id, auth=(self.username, self.password), params=payload)
         # Use requests built in Raise for Status of 400
         r.raise_for_status()
 
