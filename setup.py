e4118bcb85e3048c4af8805732b64cea90733c51
William (Billy) Jenkins <bilyrocker33@gmail.com>
2016-02-09 16:27:07 -0600

    created setup.py for travisvi integration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ThousandEyesPY',
+      version='0.21',
+      description='Python API for ThousandEyes API Monitoring Service (WIP)',
+      url='http://github.com/bijenkins/ThousandEyesPY',
+      author='Billy Jenkins',
+      author_email='bilyrocker33@gmail.com',
+      license='MIT',
+      packages=['ThousandEyesPY'],
+      zip_safe=False)
